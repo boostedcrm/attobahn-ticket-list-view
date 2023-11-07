458695b993c72c2aac156b7a72507fc18ba2aa35
rahuliww <59417571+instawebworks@users.noreply.github.com>
2023-11-07 22:40:44 +0600

    final push

--- src/App.js
+++ src/App.js
@@ -1,89 +1,231 @@
-import './App.css';
-import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {Checkbox,Box} from '@mui/material';
-import { useState } from 'react';
+import "./App.css";
+import React, { useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import {
+  Checkbox,
+  Box,
+  Modal,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import { useState } from "react";
 
-import Button from '@mui/material/Button';
-import DialogComponent from './DialogComponent';
+import Button from "@mui/material/Button";
+import DialogComponent from "./DialogComponent";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "none",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 2,
+};
+
+const ZOHO = window.ZOHO;
 
 function App() {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  const [zohoInitialized, setZohoInitialized] = useState();
+  const [loading, setLoading] = useState(true);
+  const [entityId, setEntityId] = useState();
 
+  const [toggle, setToggle] = useState(false);
 
-  const [dialogOpen, setDialogOpen] = useState(false)
+  const [selectedArray, setSelectedArray] = useState([]);
+  const [ticketList, setTicketList] = useState([]);
 
+  useEffect(() => {
+    ZOHO.embeddedApp.on("PageLoad", async function (data) {
+      setLoading(true);
+      // console.log(data);
+      setEntityId(data?.EntityId);
+      let func_name = "Zoho_desk_ticket_handle_from_milestones";
+      let req_data = {
+        get_tickets: true,
+        milestone_id: data?.EntityId,
+      };
+      await ZOHO.CRM.FUNCTIONS.execute(func_name, req_data).then(
+        async function (result) {
+          // console.log(result);
+          let resp = JSON.parse(result?.details?.output);
+          setTicketList(resp?.list || []);
+          setLoading(false);
+        }
+      );
+    });
 
+    ZOHO.embeddedApp.init().then(() => {
+      setZohoInitialized(true);
+    });
+  }, []);
 
-  const handleDialogClose = () => {
-    setDialogOpen(false);
-    window.location.reload()
+  useEffect(() => {
+    const fetchUpdateData = async () => {
+      setLoading(true);
+      // console.log(data);
+      let func_name = "Zoho_desk_ticket_handle_from_milestones";
+      let req_data = {
+        get_tickets: true,
+        milestone_id: entityId,
+      };
+      await ZOHO.CRM.FUNCTIONS.execute(func_name, req_data).then(
+        async function (result) {
+          // console.log(result);
+          let resp = JSON.parse(result?.details?.output);
+          setTicketList(resp?.list || []);
+          setLoading(false);
+        }
+      );
+    };
+
+    fetchUpdateData();
+  }, [toggle]);
+
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => {
+    setOpenModal(false);
   };
-  const [selectedArray, setSelectedArray] = useState([])
   return (
     <div className="App">
-    <Box sx={{display: "flex", justifyContent: "flex-end"}}>
-     {
-      selectedArray.length > 0 && <Button variant="contained" className='btn' onClick={() => setDialogOpen(true)}>Update</Button>
-     } 
-    </Box>
-      <TableContainer component={Paper} sx={{width: "max-width"}}>
-        <Table>
-          <TableHead className='head'>
-            <TableRow>
-              <TableCell className='box' width={10}></TableCell>
-              <TableCell className='box' width={100}>Ticket Number</TableCell>
-              <TableCell className='box' width={300}>Subject</TableCell>
-              <TableCell className='box'>Classification</TableCell>
-              <TableCell className='box'>Owner</TableCell>
-              <TableCell className='box'>Priority</TableCell>
-              <TableCell className='box'>Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell className='box'><Checkbox {...label}  width={10} onChange={(e) => {
-                if(selectedArray.length === 0){
-                  setSelectedArray(["1"])
-                }else{
-                  setSelectedArray([])
-                }
-              }} /></TableCell>
-              <TableCell className='box' width={100}><a href="#">Ticket 123</a></TableCell>
-              <TableCell className='box' width={300}>Subject</TableCell>
-              <TableCell className='box'>Classification </TableCell>
-              <TableCell className='box'>David Lee</TableCell>
-              <TableCell className='box'><span className="small">High</span></TableCell>
-              <TableCell className='box'>
-              Open
+      {loading ? (
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 20 }}>
+          <Typography> Loading... </Typography>
+          <CircularProgress size={20} />
+        </Box>
+      ) : (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              height: 50,
+              mt: 1,
+            }}
+          >
+            {selectedArray.length > 0 && (
+              <Button
+                size="small"
+                variant="contained"
+                className="btn"
+                sx={{ height: 30 }}
+                onClick={handleOpenModal}
+              >
+                Update Ticket
+              </Button>
+            )}
+          </Box>
+          <TableContainer component={Paper} sx={{ width: "max-width" }}>
+            <Table>
+              <TableHead className="head">
+                <TableRow>
+                  <TableCell className="box" width={10}></TableCell>
+                  <TableCell className="box" width={100}>
+                    Ticket Number
+                  </TableCell>
+                  <TableCell className="box" width={300}>
+                    Subject
+                  </TableCell>
+                  <TableCell className="box" width={110}>
+                    Classification
+                  </TableCell>
+                  <TableCell className="box" width={150}>
+                    Owner
+                  </TableCell>
+                  <TableCell className="box" width={70}>
+                    Priority
+                  </TableCell>
+                  <TableCell className="box" width={90}>
+                    Status
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ticketList?.map((item, index) => (
+                  <TableRow>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      <Checkbox
+                        {...label}
+                        width={10}
+                        onChange={(event) => {
+                          if (event.target.checked) {
+                            setSelectedArray([...selectedArray, item.id]);
+                          } else {
+                            setSelectedArray(
+                              selectedArray?.filter((el) => item.id !== el)
+                            );
+                          }
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      <a href={item?.webUrl}>{item.ticketNumber}</a>
+                    </TableCell>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      {item.subject}
+                    </TableCell>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      {item.classification}
+                    </TableCell>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      {/* {item.ticketNumber} */}
+                    </TableCell>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      <span className="small">{item.priority}</span>
+                    </TableCell>
+                    <TableCell sx={{ p: "0 4px" }} className="box">
+                      {item.status}
+                    </TableCell>
+                  </TableRow>
+                ))}
+
+                {/* <TableRow>
+              <TableCell className="box">
+                <Checkbox {...label} />
               </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className='box' width={10}><Checkbox {...label} /></TableCell>
-              <TableCell className='box'  width={100}><a href="#">Ticket 123</a></TableCell>
-              <TableCell className='box' width={300}>Subject</TableCell>
-              <TableCell className='box'>Classification </TableCell>
-              <TableCell className='box'>Robert Lee</TableCell>
-              <TableCell className='box'><span className="small">High</span></TableCell>
-              <TableCell className='box'>
-              Open
+              <TableCell className="box">
+                <a href="#">Ticket 123</a>
               </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-      {
-        console.log({dialogOpen})
-      }
-      <DialogComponent handleClose={handleDialogClose} dialogOpen={dialogOpen} />
+              <TableCell className="box">Subject</TableCell>
+              <TableCell className="box">Classification </TableCell>
+              <TableCell className="box">Robert Lee</TableCell>
+              <TableCell className="box">
+                <span className="small">High</span>
+              </TableCell>
+              <TableCell className="box">Open</TableCell>
+            </TableRow> */}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <Modal
+            open={openModal}
+            onClose={handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <DialogComponent
+                handleClose={handleCloseModal}
+                tickets={selectedArray}
+                setToggle={setToggle}
+                toggle={toggle}
+              />
+            </Box>
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
--- src/DialogComponent.jsx
+++ src/DialogComponent.jsx
@@ -1,57 +1,89 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import InputLabel from '@mui/material/InputLabel';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import InputLabel from "@mui/material/InputLabel";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function DialogComponent({ handleClose, dialogOpen}) {
-  const [age, setAge] = React.useState('');
+const ZOHO = window.ZOHO;
+
+export default function DialogComponent({
+  handleClose,
+  tickets,
+  setToggle,
+  toggle,
+}) {
+  const [status, setStatus] = React.useState("");
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setStatus(event.target.value);
+  };
+
+  const handleUpdate = async () => {
+    if (!status) {
+      return;
+    }
+    console.log(tickets);
+
+    let func_name = "Zoho_desk_ticket_handle_from_milestones";
+    let req_data = {
+      update_tickets: true,
+      tickets: JSON.stringify(tickets),
+      status: status,
+    };
+    await ZOHO.CRM.FUNCTIONS.execute(func_name, req_data).then(async function (
+      result
+    ) {
+      // console.log(result);
+      let resp = JSON.parse(result?.details?.output);
+      console.log(resp);
+      if (resp?.status === "success") {
+        setTimeout(() => {
+          setToggle(!toggle);
+          handleClose();
+        }, 2000);
+      }
+    });
   };
 
   return (
     <React.Fragment>
-      <Dialog
-        open={dialogOpen}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Change the status of the Tickets."}</DialogTitle>
-        <DialogContent>
-        <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Status</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Open</MenuItem>
-          <MenuItem value={20}>On Hold</MenuItem>
-          <MenuItem value={30}>Closed</MenuItem>
-        </Select>
-      </FormControl>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button onClick={handleClose}>Save</Button>
-        </DialogActions>
-      </Dialog>
+      <DialogTitle>{"Change the status of the Tickets."}</DialogTitle>
+
+      <DialogContent>
+        <FormControl sx={{ mt: 2 }} fullWidth>
+          <InputLabel id="demo-simple-select-label">Status</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={status}
+            label="Status"
+            onChange={handleChange}
+          >
+            <MenuItem value={"Open"}>Open</MenuItem>
+            <MenuItem value={"On Hold"}>On Hold</MenuItem>
+            <MenuItem value={"Escalated"}>Escalated</MenuItem>
+            <MenuItem value={"Closed"}>Closed</MenuItem>
+          </Select>
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" size="small" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="contained" size="small" onClick={handleUpdate}>
+          Update
+        </Button>
+      </DialogActions>
     </React.Fragment>
   );
 }
