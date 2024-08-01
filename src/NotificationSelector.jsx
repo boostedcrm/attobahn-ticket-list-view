fea9465ffef17628f72402d43f77c48150787691
rahuliww <59417571+instawebworks@users.noreply.github.com>
2024-08-01 20:09:05 +0600

    push

--- src/NotificationSelector.jsx
+++ src/NotificationSelector.jsx
@@ -29,11 +29,74 @@ export default function NotificationSelector({
 
   const [vendorChecked, setVendorChecked] = React.useState(true);
 
+  const [vendorList, setVendorList] = React.useState([]);
+  const [contactList, setContactList] = React.useState([]);
+  const [selectedContacts, setSelectedContacts] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchVendor = async () => {
+      let allVendors = [];
+      await ZOHO.CRM.API.getAllRecords({
+        Entity: "Vendors",
+        sort_order: "asc",
+        per_page: 200,
+        page: 1,
+      }).then(async function (data) {
+        allVendors = data?.data || [];
+      });
+
+      await ZOHO.CRM.API.getAllRecords({
+        Entity: "Vendors",
+        sort_order: "asc",
+        per_page: 200,
+        page: 2,
+      }).then(async function (data) {
+        let ven = data?.data || [];
+        allVendors = [...allVendors, ...ven];
+      });
+
+      let filteredVendors = allVendors?.filter(
+        (item) =>
+          !item?.Vendor_Type_New?.includes("Subscription") &&
+          item?.Vendor_Status === "Approved"
+      );
+      console.log(filteredVendors);
+      setVendorList(filteredVendors);
+    };
+    fetchVendor();
+  }, []);
+
   const handleChange = (event) => {
     setVendorChecked(event.target.checked);
   };
 
+  const fetchRelatedContacts = async (id) => {
+    if (id) {
+      setSelectedContacts([]);
+      await ZOHO.CRM.API.getRelatedRecords({
+        Entity: "Vendors",
+        RecordID: id,
+        RelatedList: "Contacts",
+        page: 1,
+        per_page: 200,
+      }).then(function (data) {
+        setContactList(data?.data || []);
+      });
+    } else {
+      setContactList([]);
+      setSelectedContacts([]);
+    }
+  };
+
   const handleSendNotification = async () => {
+    let otherVendorContactEmails = [];
+    if (selectedContacts?.length > 0) {
+      selectedContacts?.forEach((el) => {
+        if (el?.Email) {
+          otherVendorContactEmails.push(el?.Email);
+        }
+      });
+    }
     // let findVendor = selectedPerson?.find(
     //   (item) => item.email === "vendor@gmail.com"
     // );
@@ -67,14 +130,15 @@ export default function NotificationSelector({
     //   (item) => item.email !== "vendor@gmail.com"
     // );
     let func_name = "Zoho_desk_ticket_handle_from_milestones";
+    let selectedPersonEmails =
+      selectedPerson?.length > 0
+        ? selectedPerson?.map((item) => item?.email)
+        : [];
+    let totalEmails = [...selectedPersonEmails, ...otherVendorContactEmails];
     let req_data = {
       send_notification: true,
       tickets: JSON.stringify(ticket_id_email),
-      emails: JSON.stringify(
-        selectedPerson?.length > 0
-          ? selectedPerson?.map((item) => item?.email)
-          : []
-      ),
+      emails: JSON.stringify(totalEmails),
     };
     setLoading(true);
     await ZOHO.CRM.FUNCTIONS.execute(func_name, req_data).then(async function (
@@ -135,6 +199,45 @@ export default function NotificationSelector({
             )}
           />
         </DialogContent>
+        <DialogContent sx={{ px: 1 }}>
+          <Autocomplete
+            id="tags-outlined"
+            options={vendorList}
+            getOptionLabel={(option) => option?.Vendor_Name}
+            onChange={(event, newValue) => {
+              fetchRelatedContacts(newValue?.id);
+            }}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Vendors"
+                InputLabelProps={{ shrink: true }}
+              />
+            )}
+          />
+        </DialogContent>
+        <DialogContent sx={{ px: 1 }}>
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            // limitTags={3}
+            options={contactList}
+            getOptionLabel={(option) => option?.Full_Name}
+            value={selectedContacts}
+            onChange={(event, newValue) => {
+              setSelectedContacts(newValue);
+            }}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Vendor Contacts"
+                InputLabelProps={{ shrink: true }}
+              />
+            )}
+          />
+        </DialogContent>
         <DialogActions>
           <Button
             sx={{ width: 95, mr: 1 }}
@@ -148,7 +251,10 @@ export default function NotificationSelector({
             sx={{ width: 95 }}
             variant="contained"
             size="small"
-            disabled={selectedPerson?.length < 1 || loading}
+            disabled={
+              (selectedPerson?.length < 1 && selectedContacts?.length < 1) ||
+              loading
+            }
             onClick={handleSendNotification}
           >
             {loading ? <CircularProgress size={21} /> : "Send"}
